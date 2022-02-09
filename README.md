80b3aee0577e26d93f438d377598f7825ce7fb70
Marek Douša <68904101+dousamark@users.noreply.github.com>
2022-02-09 12:58:10 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Captour
+
+Panolens VS Marzipano
+Panolens vypadá, že je rychlejší, ale to možná závisí jen na rozlišení fotky (https://web.dev/measure/?gclid=Cj0KCQiA-eeMBhCpARIsAAZfxZCM_LMuH51B5bcbmJKZb-IcRBSfrqIn20yyQ25ATpv0MS2S-B9JmGcaAvmrEALw_wcB)
+(ozkoušet)
+
+Panolens lze snadno dát do menšího kontejneru.
+
+V Marzipanu lze přívětivě dát dohromady hotspoty v interaktivním prostředí, v Panolens jedině pomocí (viewer.add(new hotspot(x,y))). To by mělo jít snadno
+dořešit (klikáním do rozvinuté fotky a pak transformací souřadnic) ale stejně je to práce navíc.
+
+V Marzipanu je zabudovaný seznam místností - opět by mělo jít doplnit i v panolensu ale je to zase práce navíc.
+
+Marzipano má na první pocit plynulejší ovládání - panolens má nižší senzitivitu a menší dampening pohybu.
+
+Panolens se zdá být trošku víc light-weight čistě velikostí packages okolo.
---
+++ data.js
@@ -0,0 +1,449 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-balkon",
+      "name": "Balkón",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -2.4375955931849482,
+        "pitch": 0.0017889329948133081,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8794509435724063,
+          "pitch": 0.14772424356787006,
+          "rotation": 0,
+          "target": "6-pokoj1"
+        },
+        {
+          "yaw": -0.2543996199520109,
+          "pitch": 0.035644090325332556,
+          "rotation": 0,
+          "target": "7-pokoj2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-chodba1",
+      "name": "Chodba",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -0.46931650373229594,
+        "pitch": -0.027262727598264647,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5317571924485396,
+          "pitch": 0.0838863852992695,
+          "rotation": 0,
+          "target": "2-chodba2"
+        },
+        {
+          "yaw": 0.3277401723722928,
+          "pitch": 0.04043653449223683,
+          "rotation": 0,
+          "target": "7-pokoj2"
+        },
+        {
+          "yaw": 2.2260929582175404,
+          "pitch": 0.32623865829748233,
+          "rotation": 0,
+          "target": "8-zachod"
+        },
+        {
+          "yaw": -2.8257463149898054,
+          "pitch": 0.31992141977082156,
+          "rotation": 0,
+          "target": "3-koupelna"
+        },
+        {
+          "yaw": -1.5248185816231263,
+          "pitch": 0.10215561112312876,
+          "rotation": 0,
+          "target": "5-obyvak"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-chodba2",
+      "name": "Vstupní dveře",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -0.3075817142724766,
+        "pitch": 0.1588669503168454,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3420134151901806,
+          "pitch": 0.1294928761629457,
+          "rotation": 0,
+          "target": "6-pokoj1"
+        },
+        {
+          "yaw": -1.2886481501482603,
+          "pitch": 0.2008180035210323,
+          "rotation": 0,
+          "target": "1-chodba1"
+        },
+        {
+          "yaw": 1.5644575453793053,
+          "pitch": 0.13088487565116758,
+          "rotation": 0,
+          "target": "7-pokoj2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-koupelna",
+      "name": "Koupelna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 1.289089446215728,
+        "pitch": 0.47185025145275716,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.806150655938418,
+          "pitch": 0.2202281239814834,
+          "rotation": 0,
+          "target": "1-chodba1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-kuchyn",
+      "name": "Kuchyně",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 2.6894532108884874,
+        "pitch": 0.2460150605547078,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40763261348610946,
+          "pitch": 0.10119809967936355,
+          "rotation": 0,
+          "target": "5-obyvak"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-obyvak",
+      "name": "Obývací pokoj",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -2.37441420520533,
+        "pitch": 0.312681629535593,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.85569868818847,
+          "pitch": 0.09091630441930931,
+          "rotation": 0,
+          "target": "4-kuchyn"
+        },
+        {
+          "yaw": 1.7164718008036717,
+          "pitch": 0.15382624285691904,
+          "rotation": 0,
+          "target": "1-chodba1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pokoj1",
+      "name": "První ložnice",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 2.988875424478593,
+        "pitch": 0.19549463863937788,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.0890872550315684,
+          "pitch": 0.32547005144035523,
+          "rotation": 0,
+          "target": "2-chodba2"
+        },
+        {
+          "yaw": 0.07431502741206586,
+          "pitch": 0.24033745867245493,
+          "rotation": 0,
+          "target": "0-balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pokoj2",
+      "name": "Druhá ložnice",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": -0.04784020161419633,
+        "pitch": 0.07401217319803521,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.11072834792777542,
+          "pitch": 0.09349094582331219,
+          "rotation": 0,
+          "target": "2-chodba2"
+        },
+        {
+          "yaw": 2.4381782793958635,
+          "pitch": 0.02180147739390037,
+          "rotation": 0,
+          "target": "0-balkon"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-zachod",
+      "name": "Toaleta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3328,
+      "initialViewParameters": {
+        "yaw": 2.9165414794819897,
+        "pitch": 0.13337634968279488,
+        "fov": 1.459389602037366
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.767772820825245,
+          "pitch": 0.15839871289328755,
+          "rotation": 0,
+          "target": "1-chodba1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Panorama - Písečná",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
